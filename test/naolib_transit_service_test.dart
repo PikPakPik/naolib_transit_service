b32b43e5d59bec6b0c931b1061b36656c8f7ca37
Alexandre.T <alexandre.tressel@epitech.eu>
2024-08-22 17:05:47 +0200

    perf: change the analysis check file, do one test and clear pubspec.yaml

--- test/naolib_transit_service_test.dart
+++ test/naolib_transit_service_test.dart
@@ -1,12 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
-
+import 'package:test/test.dart';
 import 'package:naolib_transit_service/naolib_transit_service.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('addOne', () {
+    var calculator = Calculator();
+    expect(calculator.addOne(1), 2);
   });
 }
